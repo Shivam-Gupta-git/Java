fd6ba7a930264bf1807dd8a3a86091f0e6206e28
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-08-20 15:36:20 +0530

    Java

---
+++ src/JAVA_NOTES/4. Operators.java
@@ -0,0 +1,53 @@
+// Operator........
+
+// -> Operator = an operator is a symbol that performs a specific operation on one, two, or three operands
+// (variables/values) and produces a result.
+
+// types of operator..............
+
+// 1. Arithmetic Operators = It is Used for mathematical operation.
+//  •	+ → Addition
+//	•	- → Subtraction
+//	•	* → Multiplication
+//	•	/ → Division (quotient)
+//	•	% → Modulus (remainder)
+
+// 2. Unary Operators = It is used for Work on a single operand
+//	•	+ → Unary plus (indicates positive value)
+//	•	- → Unary minus (negates value)
+//	•	++ → Increment (increases value by 1)
+//	•	-- → Decrement (decreases value by 1)
+//	•	! → Logical NOT
+
+// 3. Assignment Operators = It is Used to assign values.
+//	•	= → Simple assignment
+//	•	+= → Add and assign
+//	•	-= → Subtract and assign
+//	•	*= → Multiply and assign
+//	•	/= → Divide and assign
+//	•	%= → Modulus and assign
+
+// 4. Relational (Comparison) Operators = It is Used to compare two values.
+//	•	== → Equal to
+//	•	!= → Not equal to
+//	•	> → Greater than
+//	•	< → Less than
+//	•	>= → Greater than or equal to
+//	•	<= → Less than or equal to
+
+// 5. Logical Operators = It is Used for logical conditions.
+//	•	&& → Logical AND (T-T = T, T-F = F, F-T = F, F-F = F)
+//	•	|| → Logical OR  (T-T = T, T-F = T, F-T = T, F-F = F)
+//	•	! → Logical NOT
+
+// 6. Bitwise Operators = It is Work on bits (0 and 1).
+//	•	& → Bitwise AND
+//	•	| → Bitwise OR
+//	•	^ → Bitwise XOR
+//	•	~ → Bitwise Complement
+//	•	<< → Left shift
+//	•	>> → Right shift
+//	•	>>> → Unsigned right shift
+
+//7. Ternary Operator = It is used to Short form of if-else.
+//Syntax: condition ? value_if_true : value_if_false
---
+++ src/JAVA_PROGRAMS/Topics/operators_7.java
@@ -0,0 +1,70 @@
+package JAVA_PROGRAMS.Topics;
+
+public class operators_7 {
+    public static void main(String[] args) {
+//      Arithmetic Operators........
+//  ->  Binary operator.
+//        int num1 = 5;
+//        int num2 = 10;
+//        System.out.println("sum of two number: " + (num1 + num2));
+//        System.out.println("subtraction of two number: " + (num1 - num2));
+//        System.out.println("multiplication of two number: " + (num1 * num2));
+//        System.out.println("module(reminder) of two number: "+ (num1 % num2));
+
+//      unary operator.
+//  ->  pre-increment
+//        int a = 10;
+//        int b = ++a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//  ->  post-increment
+//        int a = 22;
+//        int b = a++;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//  ->  pre-decrement
+//        int a = 12;
+//        int b = --a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//  ->  post-decrement
+//        int a = 43;
+//        int b = a--;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//  ->  relational operator
+//        int a = 22;
+//        int b = 21;
+//        System.out.println((a == b));
+//        System.out.println((a != b));
+//        System.out.println((a > b));
+//        System.out.println((a >= b));
+//        System.out.println((a < b));
+//        System.out.println((a <= b));
+
+//  ->  logical operator
+//        int a = 10;
+//        int b = 12;
+//        int c = 22;
+//        System.out.println(a > b && a > c);
+//        System.out.println(a > b && a < c);
+//        System.out.println(a < b || a < c);
+//        System.out.println(a < b || a > c);
+//        System.out.println(!(a > b));
+//        System.out.println(!(b > a));
+
+//  ->  Assignment Operators
+//        int a = 12;
+//        System.out.println(a += 10);
+//        System.out.println(a -= 5);
+//        System.out.println(a *= 3);
+//        System.out.println(a /= 4);
+//        System.out.println(a %= 5);
+
+
+    }
+}
