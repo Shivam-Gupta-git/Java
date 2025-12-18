f61ff97e747d91e6318cfe90d2784424288ed40d
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-18 12:05:27 +0530

    Conditional Statement

--- src/JAVA_PROGRAMS/Topics/Conditional_Statement_8/Conditional_Statement_2.java
+++ src/JAVA_PROGRAMS/Topics/Conditional_Statement_8/Conditional_Statement_2.java
@@ -1,4 +1,19 @@
 package JAVA_PROGRAMS.Topics.Conditional_Statement_8;
 
+import java.util.Scanner;
+
 public class Conditional_Statement_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your salary: ");
+        int salary = sc.nextInt();
+
+        if(salary >= 24000){
+            salary += 2000;
+        }else {
+            salary += 1000;
+        }
+        System.out.println(salary);
+    }
 }
