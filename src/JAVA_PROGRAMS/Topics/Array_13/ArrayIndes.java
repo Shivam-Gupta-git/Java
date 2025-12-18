68397636054064dff0c95c9bc7b94efd904da949
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-18 12:04:25 +0530

    Array

---
+++ src/JAVA_PROGRAMS/Topics/Array_13/ArrayIndes.java
@@ -0,0 +1,13 @@
+package JAVA_PROGRAMS.Topics.Array_13;
+
+public class ArrayIndes {
+
+    public static void main(String[]args){
+       int A1[]={1,2,3};
+       int A2[]={1,2,3};
+       if(A1.equals(A2)){
+           System.out.println("Same");
+       }else
+           System.out.println("Not");
+    }
+}
---
+++ src/JAVA_PROGRAMS/Topics/Conditional_Statement_8/Conditional_Statement_2.java
@@ -0,0 +1,4 @@
+package JAVA_PROGRAMS.Topics.Conditional_Statement_8;
+
+public class Conditional_Statement_2 {
+}
