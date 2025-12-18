50077d57b2f3c1cba656201c45cfa1c9a907fc0e
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-18 12:08:07 +0530

    Bit Manipulation

--- src/JAVA_NOTES/10. Bit manipulation.java
+++ src/JAVA_NOTES/10. Bit manipulation.java
@@ -1,1 +1,8 @@
-package JAVA_NOTES;
+// -> Bit manipulation = directly working with the binary representation of numbers using bitwise operators.
+/*
+-> Binary AND (&)
+ 0 & 0 = 0
+ 0 & 1 = 0
+ 1 & 0 = 0
+ 1 & 1 = 1
+ */
