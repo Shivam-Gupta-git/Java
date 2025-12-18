21353c82298932b772e8f13096f78c1f6a63a98f
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-18 12:06:01 +0530

    Inputs

--- src/JAVA_PROGRAMS/Topics/Inputs_3/inputs.java
+++ src/JAVA_PROGRAMS/Topics/Inputs_3/inputs.java
@@ -6,27 +6,27 @@ public class inputs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        next can be used to take string value only with the words.
+//        next can be used to take string value only with the words.....
 //        String input = sc.next();
 //        System.out.println(input);
 
-//        next can be used to take string value only with the sentences.
+//        next can be used to take string value only with the sentences......
 //        String sentence = sc.nextLine();
 //        System.out.println(sentence);
 
-//        nextInt can be used to take integer value.
+//        nextInt can be used to take integer value........
 //        int number = sc.nextInt();
 //        System.out.println(number);
 
-//        nextFloat can be used to take floating value.
+//        nextFloat can be used to take floating value.........
 //        float number = sc.nextFloat();
 //        System.out.println(number);
 
-//        nextByte can be used to take byte value.
+//        nextByte can be used to take byte value.........
 //        byte number = sc.nextByte();
 //        System.out.println(number);
 
-//        nextDouble can be used to take double value.
+//        nextDouble can be used to take double value..........
 //        double number = sc.nextDouble();
 //        System.out.println(number);
 
