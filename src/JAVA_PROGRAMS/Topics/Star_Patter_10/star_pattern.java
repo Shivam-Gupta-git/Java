35f005e9ef50394abf121a255cdd597422548385
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-21 10:23:51 +0530

    Star Pattern Questions

--- src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
+++ src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
@@ -61,9 +61,17 @@ public class star_pattern {
 //            System.out.println();
 //        }
 
-
-
-
+// Q. Pyramid Pattern.............................
+        int num = 5;
+        for (int i = 1; i <= num; i++){
+            for (int j = 1; j <= (num - i); j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
 
