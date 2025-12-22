7d42b8148a560d80acea63d2e68ad026f0984ab5
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-22 19:19:04 +0530

    Star Pattern Questions

--- src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
+++ src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
@@ -62,16 +62,71 @@ public class star_pattern {
 //        }
 
 // Q. Pyramid Pattern.............................
+//        int num = 5;
+//        for (int i = 1; i <= num; i++){
+//            for (int j = 1; j <= (num - i); j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * i - 1); k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+// Q. Inverted Pyramid Pattern..................................
+//        int num = 5;
+//        for (int i = num; i >= 1; i--){
+//            for (int j = 1; j <= (num - i); j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * i - 1); k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+// Q. Diamond Pattern................................
+//        int num = 5;
+//        for (int i = 1; i <= num; i++){
+//            for (int j = 1; j <= (num - i); j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * i - 1); k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = num; i >= 1; i--){
+//            for (int j = 1; j <= (num - i); j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= (2 * i - 1); k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+// Q. Hourglass Pattern......................
         int num = 5;
-        for (int i = 1; i <= num; i++){
-            for (int j = 1; j <= (num - i); j++){
+        for (int i = num; i >= 1; i--){
+            for (int j = 1; j <= num - i; j++){
                 System.out.print(" ");
             }
-            for (int k = 1; k <= (2 * i - 1); k++){
+            for (int k = 1; k <= 2 * i - 1; k++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        for (int i = 2; i <= num; i++){
+            for (int j = 1; j <= num - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
 
 
