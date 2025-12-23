91cd3563330a39a1a0f078b7ce293e8f8927b8e6
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-23 11:26:21 +0530

    Star Pattern Questions

--- src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
+++ src/JAVA_PROGRAMS/Topics/Star_Patter_10/star_pattern.java
@@ -107,29 +107,28 @@ public class star_pattern {
 //        }
 
 // Q. Hourglass Pattern......................
-        int num = 5;
-        for (int i = num; i >= 1; i--){
-            for (int j = 1; j <= num - i; j++){
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= 2 * i - 1; k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int i = 2; i <= num; i++){
-            for (int j = 1; j <= num - i; j++){
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= 2 * i - 1; k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        int num = 5;
+//        for (int i = num; i >= 1; i--){
+//            for (int j = 1; j <= num - i; j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= 2 * i - 1; k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 2; i <= num; i++){
+//            for (int j = 1; j <= num - i; j++){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= 2 * i - 1; k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
 
-
-
+// Q. Right-Angled Triangle....................................
 //    int num = 5;
 //    for (int i = 1; i <= num; i++){
 //        for (int j = 1; j <= i; j++){
