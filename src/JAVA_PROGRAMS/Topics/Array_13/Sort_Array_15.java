3c299e1869fe5d5daa66d58122f83ec328615f58
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-12-31 16:15:52 +0530

    Array

--- src/JAVA_PROGRAMS/Topics/Array_13/Sort_Array_15.java
+++ src/JAVA_PROGRAMS/Topics/Array_13/Sort_Array_15.java
@@ -1,8 +1,26 @@
 package JAVA_PROGRAMS.Topics.Array_13;
 
+import java.util.Arrays;
+
 public class Sort_Array_15 {
 // -> Write a program to sort an array in ascending and descending order (without using Arrays.sort).
+public static void sortArray(int[] num){
+
+    for (int i = 0; i < num.length; i++){
+        for (int j = 1 + i; j < num.length; j++){
+            if(num[i] > num[j]){
+                int temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+        }
+    }
+    System.out.println(Arrays.toString(num));
+}
+
     public static void main(String[] args) {
+        int[] number = {1, 3, 2, 7};
+        sortArray(number);
 
     }
 }
--- src/JAVA_PROGRAMS/Topics/Array_13/testing.java
+++ src/JAVA_PROGRAMS/Topics/Array_13/testing.java
@@ -1,20 +1,27 @@
 package JAVA_PROGRAMS.Topics.Array_13;
 
+import java.util.Arrays;
+
 public class testing {
-    public static int test(int[] num){
-        int st = 0;
-        System.out.println(st);
-        int end = num.length - 1;
-        System.out.println(end);
-        int middle;
-        middle = (st + end) / 2;
-        System.out.println(middle);
-        return  middle;
+    public static boolean test(int[] num){
+        for (int i = 0; i < num.length; i++){
+           for (int j = 1 + i; j < num.length; j++){
+               if(num[i] > num[j]){
+                  return false;
+               }
+           }
+        }
+        return  true;
     }
 
     public static void main(String[] args) {
-        int[] number = {2, 4};
-        int testing = test(number);
-        System.out.println(testing);
+        int[] number = {1, 2, 3, 5, 4};
+        boolean result = test(number);
+        if(!result){
+            System.out.println("isSorting not");
+        }else {
+            System.out.println("isSorting");
+        }
+
     }
 }
