8ab6ae93996009b4692a3a1de6f216d501d60d2c
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-04 22:34:52 +0530

    Remove Duplicate Element

--- src/JAVA_PROGRAMS/Topics/Array_13/Remove_duplicate_element_24.java
+++ src/JAVA_PROGRAMS/Topics/Array_13/Remove_duplicate_element_24.java
@@ -1,4 +1,23 @@
 package JAVA_PROGRAMS.Topics.Array_13;
 
 public class Remove_duplicate_element_24 {
+    public static void removeDuplicateElement(int[] num){
+
+        for (int i = 0; i < num.length; i++){
+            for (int j = 1 + i; j < num.length; j++){
+                if(num[i] == num[j]){
+                    num[j] = -1;
+                }
+            }
+        }
+        for (int i = 0; i < num.length; i++){
+            if(num[i] != -1){
+                System.out.println(num[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] number = {1, 3, 4, 4, 5};
+        removeDuplicateElement(number);
+    }
 }
