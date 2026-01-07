09eaeed74d0ef075833a4209fd36c65a41c1d4ca
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-07 22:19:32 +0530

    K Rotation

--- src/JAVA_PROGRAMS/Topics/Array_13/K_rotation_26.java
+++ src/JAVA_PROGRAMS/Topics/Array_13/K_rotation_26.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 
 public class K_rotation_26 {
     public static void kRotation(int[] num) {
-        int position = 2;
+        int position = 3;
 
         int first = num[position];
         for (int i = position; i > 0; i--) {
+            System.out.println(i);
             num[i] = num[i - 1];
+            System.out.println(num[i]);
         }
         num[0] = first;
         System.out.println(Arrays.toString(num));
     }
 
     public static void main(String[] args) {
-        int[] number = {1, 2, 3, 4, 5};
+        int[] number = {1, 3, 5, 1,2,3, 6};
         kRotation(number);
 
     }
