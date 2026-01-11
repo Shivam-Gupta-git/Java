544de80cf5747a64cfd9793601cf0889f76c8ab1
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-11 15:55:08 +0530

    Testing

--- src/JAVA_PROGRAMS/Topics/Array_13/testing.java
+++ src/JAVA_PROGRAMS/Topics/Array_13/testing.java
@@ -4,23 +4,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class testing {
+
     public static void test(int[] num) {
-        int index = 0;
-    for (int i = 0; i < num.length; i++){
-        if(num[i] != 0){
-            num[index] = num[i];
-            index ++;
+        for (int i = 0; i < num.length; i++){
+            for (int j = 1 + i; j < num.length; j++){
+                if(num[i] == num[j]){
+                    System.out.print(num[i] + " ");
+                    break;
+                }
+            }
         }
     }
-        System.out.println(index);
-    while (index < num.length){
-        num[index] = 0;
-        index ++;
-    }
-        System.out.println(Arrays.toString(num));
-    }
     public static void main(String[] args) {
-        int[] number = {1, 0, 3, 0, 5, 2};
+        int[] number = {1, 2, 2, 3, 4, 1, 5};
         test(number);
     }
 }
